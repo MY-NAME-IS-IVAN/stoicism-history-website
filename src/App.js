1edb2e5e4802a2f644f2fd9ef9e227dd1fe2e56c
Ivan <myfirstnameisivan@gmail.com>
2024-07-27 19:32:35 -0700

    Start screen quote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { StartScreen } from './components/start-screen/StartScreen';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <StartScreen />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/start-screen/StartScreen.jsx
@@ -0,0 +1,39 @@
+import './StartScreen.css';
+import React, { useState } from 'react';
+
+export const StartScreen = () => {
+  const [screenToDisappear, setScreenToDisappear] = useState(false);
+
+  return (
+    <div
+      className={`start-quote-screen ${screenToDisappear ? 'disappear' : ''}`}
+      onClick={() => setScreenToDisappear(true)}
+    >
+      <div className='start-quote'>
+        <div className='quote-line'>
+          <div className='line-text'>
+            <p>Waste no more time</p>
+            <p className='line-text-white'>Waste no more time</p>
+          </div>
+          <div className='black-stand'></div>
+        </div>
+        <div className='quote-line'>
+          <p className='line-text'>arguing what</p>
+          <div className='black-stand'></div>
+        </div>
+        <div className='quote-line'>
+          <p className='line-text'>a good man should be.</p>
+          <div className='black-stand'></div>
+        </div>
+        <div className='quote-line'>
+          <div className='line-text'>
+            <p>Be One.</p>
+            <p className='line-text-white'>Be One.</p>
+          </div>
+          <div className='black-stand'></div>
+        </div>
+      </div>
+      <p className='author'>- Marcus Aurelius</p>
+    </div>
+  );
+};
